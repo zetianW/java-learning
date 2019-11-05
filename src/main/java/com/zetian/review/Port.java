f17eee176b547e41874a64fae7d690e35d4769e8
zetian <zetian_w@163.com>
2019-11-05 17:58:49 +0800

    新建了一个review类，里面创建了一个关于简单线程输出的类

---
+++ src/main/java/com/zetian/review/Port.java
@@ -0,0 +1,18 @@
+package com.zetian.review;
+
+/**
+ * Description
+ *
+ * @author Zetian Wang
+ * @date 2019/11/05
+ **/
+public class Port implements Runnable {
+
+    @Override
+    public void run() {
+        int j = 10;
+        for (int i = 0; i < j; i++) {
+            System.out.println(Thread.currentThread().getName() + " " + i);
+        }
+    }
+}
---
+++ src/main/java/com/zetian/review/ThreadPractive.java
@@ -0,0 +1,19 @@
+package com.zetian.review;
+
+/**
+ * Description
+ *
+ * @author Zetian Wang
+ * @date 2019/11/05
+ **/
+public class ThreadPractive {
+    public static void main(String[] args) {
+        Port port = new Port();
+        Thread thread1 = new Thread(port);
+        thread1.setName("thread1");
+        Thread thread2 = new Thread(port);
+        thread2.setName("thread2");
+        thread1.start();
+        thread2.start();
+    }
+}
